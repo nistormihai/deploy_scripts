cf2ed93705e58f0f13c6350732de00a4bb0bb472
actionless <actionless.loveless@gmail.com>
2014-05-27 10:58:13 +0200

    liveblog: fixed behave script - 3

--- liveblog/behave.sh
+++ liveblog/behave.sh
@@ -8,13 +8,11 @@ test_path=$plan_path/../../liveblog/rest_tests
 root_path=/var/opt/instances/"$1"
 
 
-rm -fr $test_path
-cp -rf ./liveblog_tests $test_path
-
 [ ! -f "$root_path"/env_test/bin/activate ] && (
     virtualenv -p python3 "$root_path"/env_test;
 )
 . "$root_path"/env_test/bin/activate;
+
 
 cd "$test_path"
 
@@ -22,17 +20,20 @@ cd "$test_path"
 pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 
+
 echo SERVER_URL = \"http://$1.lb-test.sourcefabric.org/\" > $test_path/settings_local.py
 cat $test_path/settings_local.py
+
 
 # clean-up test results
 rm $plan_path/behave_results/  2>/dev/null
 mkdir $plan_path/behave_results/  2>/dev/null
 
+
 # run tests
 behave --junit --junit-folder=$plan_path/behave_results &&
 
-exit 0
 
+exit 0
 #touch -t $(date -d "1 sec" +%Y%m%d%H%M.%S) $test_path/results/tests.xml
 #ls -l $plan_path/results/tests.xml
