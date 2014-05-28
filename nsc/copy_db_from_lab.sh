33ab997b07fef681ad161a2cd08a25974114f4be
actionless <actionless.loveless@gmail.com>
2014-05-28 13:57:50 +0200

    nsc: copy backup: now LAB_INSTANCE variable stores full lab's instance name (ie 'wacsi-dev')

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -6,7 +6,7 @@ test ! -z "$bamboo_lab" && LAB_INSTANCE="$bamboo_lab" || LAB_INSTANCE="prelive"
 PORT="$bamboo_port"
 
 BACKUP_PATH="/var/dumps/""$APP"
-REMOTE_PATH="/var/www/""$APP""-"$LAB_INSTANCE"/backup/backup-*.tar.gz"
+REMOTE_PATH="/var/www/"$LAB_INSTANCE"/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
 ssh -p$PORT lab@"$DEVELOPER".sourcefabric.net -x "sudo mkdir -p /var/dumps/$APP"
