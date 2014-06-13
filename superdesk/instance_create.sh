b456635d0b63970c31ee4e3cb47cc9f6e54e4100
actionless <actionless.loveless@gmail.com>
2014-06-13 17:40:24 +0200

    superdesk: celery: use int instead of db name - 2

--- superdesk/instance_create.sh
+++ superdesk/instance_create.sh
@@ -10,6 +10,7 @@ INSTANCE="$1"
 SERVER_NAME="$2"
 NGINX_PORT=82 # frontend port of nginx
 APP_STARTING_PORT=9090 # starting port for app
+REDIS_StARTING_ID=2
 ROOT=/var/opt/superdesk_instances # superdesk instances root
 PWD=$(dirname $0)
 INSTANCE_ROOT=$ROOT/$INSTANCE
@@ -25,6 +26,12 @@ rm $INSTANCE_ROOT/.port 2>/dev/null ;
 PORT=$(expr $(cat $ROOT/*/.port | sort -nr | head -n 1) + 1) 2>/dev/null ;
 [ $PORT -le $APP_STARTING_PORT ] && PORT=$APP_STARTING_PORT ;
 echo $PORT > $INSTANCE_ROOT/.port &&
+
+# assign free db index to celery
+rm $INSTANCE_ROOT/.redis_db_index 2>/dev/null ;
+REDIS_DB_ID=$(expr $(cat $ROOT/*/.redis_db_index | sort -nr | head -n 1) + 1) 2>/dev/null ;
+[ $REDIS_DB_ID -le $REDIS_StARTING_ID ] && REDIS_DB_ID=$REDIS_StARTING_ID ;
+echo $REDIS_DB_ID > $INSTANCE_ROOT/.redis_db_index &&
 
 # generate nginx vhost
 . $PWD/templates/nginx.tpl > /etc/nginx/sites-enabled/superdesk_$INSTANCE &&
